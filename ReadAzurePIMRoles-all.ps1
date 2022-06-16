d776451d49ed603fb6ccb6df477c66977daa47ff
JJHel <57322488+JJHelen@users.noreply.github.com>
2022-06-16 16:14:09 +0300

    Update ReadAzurePIMRoles-all.ps1
    some additions - mostly connect and reporting

--- ReadAzurePIMRoles-all.ps1
+++ ReadAzurePIMRoles-all.ps1
@@ -1,5 +1,4 @@
-Connect-Azuread
-Connect-AzAccount
+Param ($userupn)
 
 # Set array
 $PIMAllRoleResults = @()
@@ -11,6 +10,39 @@ $ResNameMG = ""
 $ResNameSub = ""
 $ResNameRG = ""
 $inherfilter = ""
+
+Function ConnectAzuread {
+	# just to reduce azureadconnectrequests
+	if  ($userupn) {
+		$aadconn = $userupn
+	}
+	elseif ($($(Get-AzureADCurrentSessionInfo -erroraction SilentlyContinue).account)) {
+		$aadconn = $(Get-AzureADCurrentSessionInfo).account
+	}
+	else {
+		Write-host "Please, give your Azure Accout that you want to use."
+		$aadconn = read-host
+	}
+	Connect-AzureAD -AccountId $aadconn
+}
+
+Function ConnectAzure {	
+	# just to reduce azureconnectrequests
+	if  ($aadconn) {
+		$azconn = $aadconn
+	}
+	if  ($userupn) {
+		$azconn = $userupn
+	}
+	elseif ($($(Get-AzContext -erroraction SilentlyContinue).account)) {
+		$azconn = $(Get-AzContext).account
+	}
+	Else {
+		write-host "Please, give your Azure Accout that you want to use."
+		$azconn = read-host
+	}
+	Connect-AzAccount -AccountId $azconn
+}
 
 Function FindRoles{
 	Write-Host "working with $ResName"
@@ -109,109 +141,111 @@ Function FindRoles{
 			UsmApprovalRule_RoleSettings = $RoleSettings.UserMemberSettings | ForEach-Object {if ($PSItem -match 'ApprovalRule'){$PSItem -replace '(?<!\x0d)\x0a','' -replace '}}','}' -replace 'class AzureADMSPrivilegedRuleSetting {  RuleIdentifier: ',''}}
 			UsmAcrsRule_RoleSettings = $RoleSettings.UserMemberSettings | ForEach-Object {if ($PSItem -match ' AcrsRule'){$PSItem -replace '(?<!\x0d)\x0a','' -replace '}}','}' -replace 'class AzureADMSPrivilegedRuleSetting {  RuleIdentifier: ',''}}
 			UsmAttributeConditionRule_RoleSettings = $RoleSettings.UserMemberSettings | ForEach-Object {if ($PSItem -match 'AttributeConditionRule'){$PSItem -replace '(?<!\x0d)\x0a','' -replace '}}','}' -replace 'class AzureADMSPrivilegedRuleSetting {  RuleIdentifier: ',''}}
-		}
-			            
+
+			}
+		$item >> ".\txt\PIMRoles-$Restype-$inherfilter-$(get-date -f yyyy-MM-dd).txt"
+		
+        
 		# Add PS Object to array
-        	$PIMRoleResults += $item
+        $PIMRoleResults += $item
+		#$PIMAllRoleResults  += $item
 	}
 	Return $PIMRoleResults
 }
 
 Function GManagementGroups {
-  # Set array
-  $PIMRoleResultsMG = @()
+# Set array
+$PIMRoleResultsMG = @()
 
-  # Get all management groups
-  $MgmtGroups = Get-AzManagementGroup
-  
-  # Loop through all Management Groups
-  foreach ($MgmtGrp in $MgmtGroups) {
-    $Restype = "ManagementGroup"
-    $ResName = $MgmtGrp.displayname
-    $ResNameMG = $MgmtGrp.displayname
-    $ResId = $MgmtGrp.name
-    $FindString = "ExternalId eq '/providers/Microsoft.Management/managementGroups/$ResId'"
+# Get all management groups
+$MgmtGroups = Get-AzManagementGroup
+# Loop through all Management Groups
+foreach ($MgmtGrp in $MgmtGroups) {
+	$Restype = "ManagementGroup"
+	$ResName = $MgmtGrp.displayname
+	$ResNameMG = $MgmtGrp.displayname
+	$ResId = $MgmtGrp.name
+	$FindString = "ExternalId eq '/providers/Microsoft.Management/managementGroups/$ResId'"
+	
+	$PIMRoleResultsMG += FindRoles
+}
 
-    $PIMRoleResultsMG += FindRoles
-  }
-
-  $PIMRoleResultsMG | Export-Csv -Encoding 'UTF8' -Force -Path ".\Azure$Restype-PIMRoles-$inherfilter-$(get-date -f yyyy-MM-dd-HHmm).csv"
-  Return $PIMRoleResultsMG
+$PIMRoleResultsMG | Export-Csv -Encoding 'UTF8' -NoTypeInformation -Force -Path ".\done\Azure$Restype-PIMRoles-$inherfilter-$(get-date -f yyyy-MM-dd-HHmm).csv"
+$PIMRoleResultsMG | Export-Csv -Encoding 'UTF8' -NoTypeInformation -Force -Path ".\latest\Azure$Restype-PIMRoles-$inherfilter.csv"
+Return $PIMRoleResultsMG
 }
 
 Function GSubscritions {
-  # Set array
-  $PIMRoleResultsSub = @()
+# Set array
+$PIMRoleResultsSub = @()
 
-  # Get all Azure Subscriptions
-  $Subs = Get-AzSubscription
-  
-  # Just want to test with set of subscriotions
-  # $currdir = Get-Location
-  # $Subs = import-csv -path $currdir\subscriptions.txt
+# Get all Azure Subscriptions
+$Subs = Get-AzSubscription
+# $currdir = Get-Location
+# $Subs = import-csv -path $currdir\subscriptions.txt
 
-  # Loop through all Subscriptions
-  foreach ($Sub in $Subs) {
-    $ResType = "Subscription"
-    $ResNameSub = $sub.name
-    $ResName = $sub.name
-    $ResId = $sub.id
-    $FindString = "ExternalId eq '/subscriptions/$ResId'"
+# Loop through all Subscriptions
+foreach ($Sub in $Subs) {
+	$ResType = "Subscription"
+	$ResNameSub = $sub.name
+	$ResName = $sub.name
+	$ResId = $sub.id
+	$FindString = "ExternalId eq '/subscriptions/$ResId'"
 
-    $PIMRoleResultsSub += FindRoles
-  }
+	$PIMRoleResultsSub += FindRoles
+}
 
-  $PIMRoleResultsSub | Export-Csv -Encoding 'UTF8' -Force -Path ".\Azure$Restype-PIMRoles-$inherfilter-$(get-date -f yyyy-MM-dd-HHmm).csv"
-  Return $PIMRoleResultsSub
+$PIMRoleResultsSub | Export-Csv -Encoding 'UTF8' -NoTypeInformation -Force -Path ".\done\Azure$Restype-PIMRoles-$inherfilter-$(get-date -f yyyy-MM-dd-HHmm).csv"
+$PIMRoleResultsSub | Export-Csv -Encoding 'UTF8' -NoTypeInformation -Force -Path ".\latest\Azure$Restype-PIMRoles-$inherfilter.csv"
+Return $PIMRoleResultsSub
 }
 
 Function GResourceGroups {
-  # Set array
-  $PIMRoleResultsRG = @()
+# Set array
+$PIMRoleResultsRG = @()
 
-  # Rerun subscription part, but only get resource groups. This can take long time (hours)
-  # Get all Azure Subscriptions
-  $Subs = Get-AzSubscription 
-  
-  # Just want to test with set of subscriotions
-  # $currdir = Get-Location
-  # $Subs = import-csv -path $currdir\subscriptions.txt
+# Get all Azure Subscriptions
+$Subs = Get-AzSubscription
+# $currdir = Get-Location
+# $Subs = import-csv -path $currdir\subscriptions.txt
 
-  # Loop through all Subscriptions
-  foreach ($Sub in $Subs) {
-    Set-AzContext $Sub.Id
-    $ResNameSUB = $sub.name
-    $ResIdSub = $Sub.id
-    $ResNameParent = $sub.name
-    $ResGroups = Get-AzResourceGroup
+# Loop through all Subscriptions
+foreach ($Sub in $Subs) {
+	Set-AzContext $Sub.Id
+	$ResNameSUB = $sub.name
+	$ResIdSub = $Sub.id
+	$ResNameParent = $sub.name
+	$ResGroups = Get-AzResourceGroup
+	
+	Write-Host "Working with Sub $ResNameSUB ResourceGroups"
 
-    Write-Host "Working with Sub $ResNameSUB ResourceGroups"
+	foreach ($RG in $ResGroups) {
+		$ResType = "ResourceGroup"
+		$ResName = $RG.ResourceGroupName
+		$ResNameRG = $RG.ResourceGroupName
+		$ResIdRG = $RG.id
+		$FindString = "ExternalId eq '/subscriptions/$ResIdSub/resourceGroups/$ResNameRG'"
 
-    foreach ($RG in $ResGroups) {
-      $ResType = "ResourceGroup"
-      $ResName = $RG.ResourceGroupName
-      $ResNameRG = $RG.ResourceGroupName
-      $ResIdRG = $RG.id
-      $FindString = "ExternalId eq '/subscriptions/$ResIdSub/resourceGroups/$ResNameRG'"
-
-      $PIMRoleResultsRG += FindRoles
-      }
-  }
-    $PIMRoleResultsRG | Export-Csv -Encoding 'UTF8' -Force -Path ".\Azure$ResType-PIMRoles-$inherfilter-$(get-date -f yyyy-MM-dd-HHmm).csv"
-    Return $PIMRoleResultsRg
+		$PIMRoleResultsRG += FindRoles
+		}
+}
+	$PIMRoleResultsRG | Export-Csv -Encoding 'UTF8' -NoTypeInformation -Force -Path ".\done\Azure$ResType-PIMRoles-$inherfilter-$(get-date -f yyyy-MM-dd-HHmm).csv"
+	$PIMRoleResultsRG | Export-Csv -Encoding 'UTF8' -NoTypeInformation -Force -Path ".\latest\Azure$ResType-PIMRoles-$inherfilter.csv"
+	Return $PIMRoleResultsRg
 }
 
+ConnectAzuread
+ConnectAzure
 $inherfilter = "noinherit"
 $PIMAllRoleResults += GManagementGroups
 $PIMAllRoleResults += GSubscritions
 $PIMAllRoleResults += GResourceGroups
-$PIMAllRoleResults | Export-Csv -Encoding 'UTF8' -Force -Path ".\AzureALL-PIMRoles-$inherfilter-$(get-date -f yyyy-MM-dd-HHmm).csv"
+$PIMAllRoleResults | Export-Csv -Encoding 'UTF8' -NoTypeInformation -Force -Path ".\done\AzureALL-PIMRoles-$inherfilter-$(get-date -f yyyy-MM-dd-HHmm).csv"
+$PIMAllRoleResults | Export-Csv -Encoding 'UTF8' -NoTypeInformation -Force -Path ".\latest\AzureALL-PIMRoles-$inherfilter.csv"
 
 $inherfilter = "inclinherit"
 $PIMAllNiRoleResults += GManagementGroups
 $PIMAllNiRoleResults += GSubscritions
-
-# getting resource groups inherited info can take long time. -- and that means many hours
 #$PIMAllNiRoleResults += GResourceGroups
-
-$PIMAllNiRoleResults | Export-Csv -Encoding 'UTF8' -Force -Path ".\AzureALL-PIMRoles-$inherfilter-$(get-date -f yyyy-MM-dd-HHmm).csv"
+$PIMAllNiRoleResults | Export-Csv -Encoding 'UTF8' -NoTypeInformation -Force -Path ".\done\AzureALL-PIMRoles-$inherfilter-$(get-date -f yyyy-MM-dd-HHmm).csv"
+$PIMAllNiRoleResults | Export-Csv -Encoding 'UTF8' -NoTypeInformation -Force -Path ".\latest\AzureALL-PIMRoles-$inherfilter.csv"
